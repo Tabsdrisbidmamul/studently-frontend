0684a0454c7f93e1a2d5e98c59c395e70c71e4dd
Idris Khan <idris.khan303@gmail.com>
2020-11-08 18:38:16 +0000

    Refactored card controller

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -9251,54 +9251,7 @@ _base.elements.overview.addEventListener('click', /*#__PURE__*/function () {
             } else if (e.target.closest('.card')) {
               (0, _cardController.cardLoader)(e); // User click 'make a new card' button in card homepage
             } else if (e.target.closest('.make-card')) {
-              (0, _cardController.cardMakerLoader)(e); // // set the boolean for card facing
-              // let cardFacing = 'question';
-              // // question box update the value in real time to the card
-              // document.querySelector('.textarea-q').addEventListener('input', (e) => {
-              //   cardView.renderCardQuestion(
-              //     document.querySelector('.card--make'),
-              //     document.querySelector('.textarea-q').value
-              //   );
-              // });
-              // // answer box update the value in real time to the card
-              // document.querySelector('.textarea-a').addEventListener('input', (e) => {
-              //   cardView.renderCardQuestion(
-              //     document.querySelector('.card--make'),
-              //     document.querySelector('.textarea-a').value
-              //   );
-              // });
-              // // swap card facing side
-              // document.querySelector('.btn--switch').addEventListener('click', (e) => {
-              //   let textareaBox = '.textarea-q';
-              //   if (cardFacing === 'question') {
-              //     textareaBox = '.textarea-q';
-              //     cardFacing = 'answer';
-              //   } else {
-              //     textareaBox = '.textarea-a';
-              //     cardFacing = 'question';
-              //   }
-              //   cardView.renderCardQuestion(
-              //     document.querySelector('.card--make'),
-              //     document.querySelector(textareaBox).value
-              //   );
-              // });
-              // // User clicks to create the card
-              // document
-              //   .querySelector('.icon--make-card-right')
-              //   .addEventListener('click', (e) => {
-              //     console.log("we're in");
-              //     const question = document.querySelector('.textarea-q').value;
-              //     const answer = document.querySelector('.textarea-a').value;
-              //     const user = storage.getObj('user') || state.user.userData.id;
-              //     state.card.createCard(question, answer, user, storage.getObj('token'));
-              //   });
-              // // User clicks to cancel the card creation
-              // document
-              //   .querySelector('.icon--make-card-left')
-              //   .addEventListener('click', (e) => {
-              //     clearOverview();
-              //     cardRender(elements.overview, state.card.cards);
-              //   });
+              (0, _cardController.cardMakerLoader)(e);
             }
 
           case 1:
@@ -9747,7 +9700,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61177" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63767" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
