bc014a7269afc108a47ce146fa886628a690cdbd
Idris Khan <idris.khan303@gmail.com>
2020-11-28 15:22:50 +0000

    SVG updates to have them working in the web app

--- dist/line-graph.8dc2f68c.js
+++ dist/line-graph.8dc2f68c.js
@@ -147,11 +147,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-<<<<<<< HEAD
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64034" + '/');
-=======
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64750" + '/');
->>>>>>> UI
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52362" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
