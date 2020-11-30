fb223c02e5b166e511de4983086e9ec240b13782
Idris Khan <idris.khan303@gmail.com>
2020-11-30 08:25:49 +0000

    SVG update

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -32552,7 +32552,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64175" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56440" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
